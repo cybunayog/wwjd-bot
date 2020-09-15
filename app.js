347ef7361210b3694f70ca079347ca7503bf4e5c
Cy Bunayog <cybunayog@gmail.com>
2020-09-15 12:59:12 -0700

    Added a verse and improved formatting

--- app.js
+++ app.js
@@ -64,14 +64,15 @@ function handleCommand(msg, cmd, args) {
                 .catch(err => {
                     console.log("rejected handleCommand(): " + err);
                     var motivationVerses = [
-                        "Trust in the LORD with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths. - Proverbs 3:5-6",
-                        "But Jesus beheld them, and said unto them, With men this is impossible; but with God all things are possible - Matthew 19:26",
-                        "But my God shall supply all your need according to his riches in glory by Christ Jesus. - Philippians 4:19"
+                        `"Trust in the LORD with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths." - Proverbs 3:5-6`,
+                        `"But Jesus beheld them, and said unto them, With men this is impossible; but with God all things are possible." - Matthew 19:26`,
+                        `"But my God shall supply all your need according to his riches in glory by Christ Jesus." - Philippians 4:19`,
+                        `"And he said unto me, My grace is sufficient for thee: for my strength is made perfect in weakness. Most gladly therefore will I rather glory in my infirmities, that the power of Christ may rest upon me." - 2 Corinthians 12:9`
                     ];
                     embed
                         .setTitle("Oops,")
                         .setDescription(
-                            `Looks like it was never written :frowning2: But here's a verse to motivate you!\n\n
+                            `Looks like I never wrote it :frowning2:\nBut here's a verse to motivate you!\n\n
                             ${motivationVerses[Math.floor(Math.random() * motivationVerses.length)]}`
                         )
                     channel.send(embed);
