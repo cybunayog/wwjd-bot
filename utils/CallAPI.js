9e374d9645b4a213a0be1d67fc2b04d7ad26e5ac
Cy Bunayog <cybunayog@gmail.com>
2020-10-19 10:40:36 -0700

    init commit

--- utils/CallAPI.js
+++ utils/CallAPI.js
@@ -22,10 +22,32 @@ export async function callBibleAPI(query) {
     });
 }
 
+// TODO: Make API endpoints dynamic
 export async function callESV(query) {
     const options = {
         'method': 'GET',
         'url': `https://api.esv.org/v3/passage/search/?q=${query}`,
+        'headers': {
+            'Authorization': `Token ${process.env.ESV_API_TOKEN}`
+        }
+    };
+    return new Promise((resolve, reject) => {
+        request(options, function (error, response) {
+            if (error) return reject(err);
+            try {
+                const apiResponse = resolve(JSON.parse(response.body));
+                return apiResponse;
+            } catch (e) {
+                reject("callESV() error: " + e);
+            }
+        })
+    });
+}
+
+export async function fetchESVPassage(query) {
+    const options = {
+        'method': 'GET',
+        'url': `https://api.esv.org/v3/passage/text/?q=${query}`,
         'headers': {
             'Authorization': `Token ${process.env.ESV_API_TOKEN}`
         }
